4fab52e8e985651507aad431fd7a2181d4a356e7
Eesa000 <liba2414@gmail.com>
2025-06-01 11:32:13 +0100

    storing birthdays

--- ;esspn10.py
+++ ;esspn10.py
@@ -1,12 +1,2 @@
-birthday1 = "2000-01-15"
-birthday2 = "1995-06-23"
-birthday3 = "1988-12-05"
-birthday4 = "2002-09-30"
-birthday5 = "1999-03-11"
-
-# Print the birthdays
-print("Rohans:", birthday1)
-print("Amys:", birthday2)
-print("Eesas:", birthday3)
-print("Shayaans:", birthday4)
-print("Mikails:", birthday5)
+property #10
+# It is designed to handle specific data processing tasks, including reading from files,
