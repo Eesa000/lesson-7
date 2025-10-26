5342665e25032c2d1f00d056276bea41bc9d26da
Eesa000 <liba2414@gmail.com>
2025-10-26 09:57:14 +0000

    shutdown.py

---
+++ shutdown.py
@@ -0,0 +1,12 @@
+def shutdown():
+    user_input = input("Do you want to shut down the system? (Yes/No): ")
+
+    if user_input == "Yes":
+        print("Shutting down...")
+    elif user_input == "No":
+        print("Shutdown aborted.")
+    else:
+        print("Sorry.")
+
+# Call the function
+shutdown()
