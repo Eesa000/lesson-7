fd9d1f7351911693949f67dd79ba2bb9ee303241
Eesa000 <liba2414@gmail.com>
2025-12-07 10:33:03 +0000

    math.py

---
+++ math.py
@@ -0,0 +1,11 @@
+import math #importing math module
+#using ceil and floor functiom of math module
+print('The Floor and Ceiling value of 23.56 are: ' + str(math.ceil(23.56)) + ', ' +
+str(math.floor(23.56)))
+
+x = 10
+y = -15
+#using copysign fucniton
+print('The value of x after copying the sign from y is: ' + str(math.fabs(-96)) + ', ' + str(math.fabs(56)))
+
+print('The GCD of 24 and 56 : ' + str(math.gcd(24, 56)))
