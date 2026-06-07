b1eb9ffc1d18554748e9ac61929aa875ab06e048
Eesa000 <liba2414@gmail.com>
2026-06-07 11:16:43 +0100

    private.py

---
+++ private.py
@@ -0,0 +1,18 @@
+# class creation
+class myclass:
+
+    # private variable
+    __privatevar = 27
+
+    # private method
+    def __privmeth(self):
+        print("i'm inside class my class")
+
+    # function to print value of private variable
+    def hello(self):
+        print("private variable value: ",self.__privatevar)
+
+# object creation and method call
+foo = myclass()
+foo.hello()
+foo.__privmeth()
