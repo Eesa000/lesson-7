881850b94ddca34e7e2cfae4dc645bb242594f42
Eesa000 <liba2414@gmail.com>
2026-06-21 11:17:57 +0100

    abstract.py

---
+++ abstract.py
@@ -0,0 +1,24 @@
+# import necessary modules
+from abc import ABC, abstractmethod
+
+# create base class
+class absclass(ABC):
+
+    # function to print a value
+    def print(self,x):
+        print("passed value: ",x)
+
+    # abstract method
+    @abstractmethod
+    def task(self):
+        print("we are inside absclass task")
+
+# create sub class
+class test_class(absclass):
+    def task(self):
+        print("we are inside test_class task")
+
+#object of test class created
+test_obj = test_class()
+test_obj.task()
+test_obj.print(100)
